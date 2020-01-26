4fe73a77eb2155c3448af41d6c27254056c7813f
John P <johnp.valerio@gmail.com>
2020-01-25 22:09:32 -0500

    Added initial JSON setup

--- src/WaterTracker.py
+++ src/WaterTracker.py
@@ -1,16 +1,26 @@
-from datetime import date
+import datetime
+from datetime import date, timedelta
 import json
 
+db = {}
+
+
+def load_db():
+    global db
+    with open('WaterInfo.json', 'r') as r_f:
+        db = json.load(r_f)
+
+
 def update_user():
-    with open('WaterInfo.JSON', 'w') as file:
-        json.dump(file, setup(), indent=4)
+    with open('WaterInfo.json', 'w', encoding='utf-8') as w_f:
+        # db = setup()
+        json.dump(db, w_f, indent=4)
+
 
 def setup():
     db = {}
-    temp = {}
-    temp['total_intake'] = 0
-    temp['chug_pb'] = 0
-    temp['freq'] = 0
-    db[date.today()] = temp
-
+    for i in reversed(range(10)):
+        temp = {'total_intake': 0, 'chug_pb': 0, 'freq': 0}
+        db[str(date.today() - timedelta(days=i))] = temp
+    # print(db)
     return db
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 
 from src.WaterReader import water_read
-from src.WaterTracker import setup
+from src.WaterTracker import setup, update_user
 
 if __name__ == '__main__':
     # output = water_read()
     # print(output)
-    setup()
+    update_user()
